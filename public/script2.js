94c877fd363b4a01ab64e556b4d2ddd042cb0f2d
Saransh019 <b20132@students.iitmandi.ac.in>
2024-04-20 14:04:47 +0530

    go back added

--- public/script2.js
+++ public/script2.js
@@ -7,18 +7,24 @@ let startTime;
 let reactionTimes = [];
 
 startTestButton.addEventListener("click", () => {
-  startTestButton.style.display = "none";
-  document.body.style.backgroundColor = "red";
-  const randomInterval = Math.floor(Math.random() * 4) + 1;
-  result.textContent = "";
-  responseButton.disabled = true;
-  responseButton.style.display = "block";
+  if (startTestButton.textContent == "Go Back") {
+    startTestButton.style.background = "yellow";
+    window.location.href = "/";
+    //redirect to homepage
+  } else {
+    startTestButton.style.display = "none";
+    document.body.style.backgroundColor = "red";
+    const randomInterval = Math.floor(Math.random() * 4) + 1;
+    result.textContent = "";
+    responseButton.disabled = true;
+    responseButton.style.display = "block";
 
-  setTimeout(() => {
-    document.body.style.backgroundColor = "green";
-    startTime = new Date();
-    responseButton.disabled = false;
-  }, randomInterval * 1000);
+    setTimeout(() => {
+      document.body.style.backgroundColor = "green";
+      startTime = new Date();
+      responseButton.disabled = false;
+    }, randomInterval * 1000);
+  }
 });
 
 responseButton.addEventListener("click", () => {
@@ -44,11 +50,12 @@ responseButton.addEventListener("click", () => {
     responseButton.style.display = "none";
     document.body.style.backgroundColor = "white";
     startTestButton.style.display = "block";
+    startTestButton.textContent="Go Back";
     reactionTimes = [];
 
-    data.name= localStorage.getItem("name");
-    data.gender= localStorage.getItem("gender");
-    data.reactionTime=averageReactionTime;
+    data.name = localStorage.getItem("name");
+    data.gender = localStorage.getItem("gender");
+    data.reactionTime = averageReactionTime;
     savv();
   }
 });
@@ -63,65 +70,28 @@ document.body.addEventListener("transitionend", () => {
 const csvFilePath = "reaction_times.csv";
 const csvHeader = "Name,Roll Number,Reaction Time\n";
 
-// function saveData(reactionTimee) {
-//   console.log("saveee");
-//   const name = localStorage.getItem("studentName");
-//     const rollNumber = localStorage.getItem("studentRollNumber");
-//   const reactionTime = reactionTimee;
-
-//   if (fs.existsSync(csvFilePath)) {
-//     console.log("!1");
-//     // Read the existing CSV file
-//     const data = fs.readFileSync(csvFilePath, "utf-8");
-
-//     // Check if the file is empty
-//     if (data === "") {
-//       // Write the header and the new data to the file
-//       fs.writeFileSync(
-//         csvFilePath,
-//         csvHeader + name + "," + rollNumber + "," + reactionTime
-//       );
-//     } else {
-//       // Add the new data to the file after the last row
-//       const newData = data
-//         .split("\n")
-//         .slice(0, -1)
-//         .concat(name + "," + rollNumber + "," + reactionTime + "\n")
-//         .join("\n");
-//       fs.writeFileSync(csvFilePath, newData);
-//     }
-//   } else {
-//     console.log("!12112");
-
-//     fs.writeFileSync(
-//       csvFilePath,
-//       csvHeader + name + "," + rollNumber + "," + reactionTime
-//     );
-//   }
-// }
-
 const data = {
-    name: null,
-    gender: null,
-    reactionTime: null
+  name: null,
+  gender: null,
+  reactionTime: null,
 };
 
-function savv(){
-    fetch('/submitData', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
+function savv() {
+  fetch("/submitData", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log("Data successfully sent to server");
+      } else {
+        console.error("Error:", response.status);
+      }
     })
-    .then(response => {
-        if (response.ok) {
-            console.log('Data successfully sent to server');
-        } else {
-            console.error('Error:', response.status);
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
+    .catch((error) => {
+      console.error("Error:", error);
     });
 }
